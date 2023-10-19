e00f57397e459dd031258a30f311fa0f35c77529
naranjujeli <lucadive@gmail.com>
2023-10-19 14:09:40 -0300

    Añadir demos de pipes: - number - slice - currency - percent - uppercase - lowercase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'pipes';
+  name: string = "Luca";
+  array: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9 ,10];
+  PI: number = Math.PI;
+  percentage: number = 0.234;
+  salary: number = 1234.5;
+  superhero: any = { // Cómo estamos con los superjiros
+    name: "Logan",
+    alias: "Wolverine",
+    edad: 500,
+    address: {
+      
+    }
+  }
 }
